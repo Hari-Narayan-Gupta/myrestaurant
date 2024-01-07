5ddebb48c86ccb1c2b51b20feb9d082c7ecec8d8
Hari-Narayan-Gupta <harinrngupta@gmail.com>
2024-01-07 09:07:54 +0530

    my restaurant commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant';
+// import UseState from './component/Hooks/useState';
+// import UseEffect from './component/Hooks/useEffect';
+// import UseReducer from './component/Hooks/useReducer';
+
+function App() {
+  return (
+    <>
+      <Resturant/>
+    </>
+    
+  );
+}
+
+export default App;
